9acd57bd4eb1e4053e6b74878c50c6fd95722336
Sabine Randow <sabine.randow@gmail.com>
2022-11-29 19:25:03 +0100

    big clean after 1 year of neglect

--- src/constants/footer/Footer.jsx
+++ src/constants/footer/Footer.jsx
@@ -1,34 +1,48 @@
 import "./footer.css";
 
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <div className="footer-container"> 
-        <hr></hr> 
-        <div className="footer">
-           
-
-           <div className="footer-box">
-               <h3>Contact details</h3>
-               <ul> 
-                   <li><a href = "mailto: sabine.randow@gmail.com"> <i class="bi bi-envelope-fill"></i> Send me an Email!</a> </li>
-                   <li> sabine.randow@gmail.com</li>
-
-               </ul>
-           </div>
-           <div className="footer-box">
-               <h3>Links</h3>
-               <ul>
-                    <li> <a href="https://www.linkedin.com/in/sabine-r-4a9217116/" target="_blank"><i class="bi bi-linkedin"></i> I'm on LinkedIn!</a> </li>
-                    <li> <a href="https://github.com/BaBineBo" target="_blank"><i class="bi bi-github"></i> I'm on GitHub!</a></li>
-               </ul>   
-           </div>
-
-
+  return (
+    <div className="footer-container">
+      <hr></hr>
+      <div className="footer container">
+        <div className="footer-box col">
+          <h3>Contact details</h3>
+          <ul>
+            <li>
+              <a href="mailto: sabine.randow@gmail.com">
+                {" "}
+                <i class="bi bi-envelope-fill"></i> Send me an Email!
+              </a>{" "}
+            </li>
+            <li> sabine.randow@gmail.com</li>
+          </ul>
         </div>
-        <hr></hr></div>
-    )
+        <div className="footer-box col">
+          <h3>Links</h3>
+          <ul>
+            <li>
+              {" "}
+              <a
+                href="https://www.linkedin.com/in/sabine-r-4a9217116/"
+                target="_blank"
+              >
+                <i class="bi bi-linkedin"></i> I'm on LinkedIn!
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="https://github.com/BaBineBo" target="_blank">
+                <i class="bi bi-github"></i> I'm on GitHub!
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr></hr>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,63 +1,92 @@
-import React from 'react';
+import React from "react";
 
 // Import componentes
-import BigName from './BigName';
-import Header from '../../constants/header/Header';
+import BigName from "./BigName";
+import Header from "../../constants/header/Header";
 
 const Main = () => {
-    return (
-        <>
-            <div className="main">
+  return (
+    <div className="cl">
+      <div className="row justify-content-center">
+        <img src="./itsAme.jpeg" alt="is pic of me" />
+      </div>
 
-                <img src="./itsAme.jpeg" alt="is pic of me"/>
-                <div className="content">
-                    <h2>Who am I?</h2>
-                    <p>I'm Sabine Randow, a 4th year student at Chalmers currently enrolled in the master program High Performance Computer Systems (MPHPC for short). 
-                        I moved to Gothenburg the day after my High School graduation 2018, and have been glued to my computer ever since. 
-                        In my spare time I'm addicted to keyboards, flowers and making pies.</p>
-                </div>
+      <div className="content row">
+        <h2>Who am I?</h2>
+        <p>
+          I'm Sabine Randow, a 5th year student at Chalmers currently enrolled
+          in the master program High Performance Computer Systems (MPHPC for
+          short). I moved to Gothenburg the day after my High School graduation
+          2018, and have been glued to my computer ever since. In my spare time
+          I'm addicted to keyboards, flowers and making pies. Currently making games in Godot and 
+          painting 2D art in Aseprite while not working on master thesis.
+        </p>
+      </div>
 
-                <div className="content">
-                    <h2>What have I done?</h2>
-                    <p>I've been at Chalmers for a while and have managed to squeeze in a lot of volunteer work for the student division, 
-                        some real work and side projects. 
-                        
-                    </p>
+      <div className="content row">
+        <h2>What have I done?</h2>
+        <p>
+          I've been at Chalmers for a while and have managed to squeeze in a lot
+          of volunteer work for the student division, some real work and side
+          projects.
+        </p>
 
-                    <div className="ul-flex">
-                        <div>
-                        <h3>What I school?</h3>
-                            <ul>
-                                <li>Bachelors in Computer Science and Engineering (Datateknik).</li>
-                                <li> Masters in High Performance Computer Systems.</li>
-                            </ul>
-                        </div>
+        <div>
+          <h3>What I school?</h3>
+          <ul>
+            <li>Bachelors in Computer Science and Engineering (Datateknik).</li>
+            <li> Masters in High Performance Computer Systems.</li>
+          </ul>
+        </div>
 
-                        <div >
-                        <h3>What I work?</h3>
-                            <ul>
-                                <li>Laid the foundation of a new webbsite and tools for Chalmers Studentkår Rekrytering (CSR)</li>
-                                <li>Was a project leader at CSR, leading recruitment-projects.</li>
-                                <li>Soon to be Student Assistant for the course in Computer system engineering (EDA333, Datorsystemteknik).</li>
-                            </ul>
-                        </div>
-                        <div >
-                        <h3>What I volunteer?</h3>
-                            <ul>
-                                <li>I'm in my second year on the division's board.</li>
-                                <li>I'm currently the head of DNS, the student educational unit. I partake in evaluations of courses and handle situations that may arise with courses.</li>
-                                <li>I was the head of DRust, a committee that takes care of the student division premises.</li>
-                                <li>I played <a href="https://en.wikipedia.org/wiki/Viola" target="_blank">viola</a> (not a violin i promise they are different) in a student orchestra/theater group called Chalmers Barockensemble</li>
-                            </ul>
-                        </div>
-                
-                    </div>
+        <div>
+          <h3>What I work?</h3>
+          <ul>
+            <li>
+              Laid the foundation of a new webbsite and tools for Chalmers
+              Studentkår Rekrytering (CSR)
+            </li>
+            <li>Was a project leader at CSR, leading recruitment-projects.</li>
+            <li>
+              Student Assistant for the course in Computer system engineering
+              (EDA333, Datorsystemteknik) 2022.
+            </li>
+            <li>Summer internship at Opera working with React and Go.</li>
+          </ul>
+        </div>
+        <div>
+          <h3>What I volunteer?</h3>
+          <ul>
+            <li>Spent 2 year on the divisions board.</li>
+            <li>
+              Was the head of DNS, the student educational unit. I attended
+              evaluations of courses and handled situations that arose with
+              courses.
+            </li>
+            <li>
+              I was the head of DRust, a committee that takes care of the
+              student division premises.
+            </li>
+            <li>
+              I played{" "}
+              <a href="https://en.wikipedia.org/wiki/Viola" target="_blank">
+                viola
+              </a>{" "}
+              (not a violin i promise they are different) in a student
+              orchestra/theater group called Chalmers Barockensemble
+            </li>
+          </ul>
+        </div>
+      </div>
 
-                </div>
+      <div className="content row">
+        <h2>What do I want?</h2>
+        <p>A workplace where I am challenged logically but get to utilize my more artsy-creative side.
+          I want friends in the office and people in the office, sometimes. I want to be part of a team
+          that tackles problems together while having fun!</p>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-        </>
-    )
-}
-
-export default Main
+export default Main;
